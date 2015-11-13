3c7d9a530bc415cc3fb0749e8c416e7155435133
ludohenin <ludovic.henin@yahoo.com>
2015-11-13 16:51:39 +0100

    Refactor builder and public API export

--- app-buildr.ts
+++ app-buildr.ts
@@ -1,12 +1,8 @@
 require('reflect-metadata/Reflect');
-
-// TODO: Move all this into src folder.
-
 import {Builder} from './src/builder';
 import {TaskLoader} from './src/loader';
 import {TaskRegistry, EventRegistry} from './src/registry';
 import {TaskRunner} from './src/runner';
-
 
 const CORE_PROVIDERS = [
   Builder,
@@ -16,31 +12,11 @@ const CORE_PROVIDERS = [
   TaskRunner
 ];
 
-
-let builder: Builder;
-
 export {EventEmitter} from 'angular2/angular2';
 export {Task} from './src/metadata';
-export function init(buildProviders: any[]): void {
-  builder = Builder.init(CORE_PROVIDERS, buildProviders);
+export function init(providers: any[]) {
+  Builder.init(CORE_PROVIDERS, providers);
 }
-export function load(path: string|string[]) {
-  isInit();
-  builder.load(path);
-}
-export function task(name: string, seq: string[]) {
-  isInit();
-  builder.task(name, seq);
-}
-export function start(task: string) {
-  isInit();
-  builder.start(task);
-}
-
-
-// ----------
-// Helper.
-function isInit() {
-  if (!builder && !(builder instanceof Builder)) {
-    throw new Error('You must run init first'); }
-}
+export let load  = Builder.load;
+export let task  = Builder.task;
+export let start = Builder.start;
--- src/builder.ts
+++ src/builder.ts
@@ -5,23 +5,25 @@ import {TaskRegistry} from './registry';
 import {TaskRunner} from './runner';
 
 
+let builderInstance: Builder;
+
 @Injectable()
 export class Builder {
   constructor(private _taskRegistry: TaskRegistry,
               private _loader: TaskLoader,
               private _runner: TaskRunner) {
   }
-  public static init(coreProviders?: any[], buildProviders?: any[]): Builder {
+  static init(coreProviders: any[], buildProviders?: any[]): void {
     let appInjector = AppInjector.init(coreProviders, buildProviders || []);
-    return appInjector.get(Builder);
+    builderInstance = appInjector.get(Builder);
   }
-  load(path: string | string[]): void {
-    this._loader.load(path);
+  static load(path: string | string[]): void {
+    builderInstance._loader.load(path);
   }
-  start(taskname: string): void {
-    this._runner.run(taskname);
+  static task(taskname: string, sequence: any[]): void {
+    builderInstance._taskRegistry.registerVirtualTask(taskname, sequence);
   }
-  task(taskname: string, sequence: any[]): void {
-    this._taskRegistry.registerVirtualTask(taskname, sequence);
+  static start(taskname: string): void {
+    builderInstance._runner.run(taskname);
   }
 }
